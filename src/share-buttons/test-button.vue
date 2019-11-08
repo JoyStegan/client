98a8bab610a0048d11ea04353e7b12f77d8c2ecd
ashirogisen <m.rizki.aiman@gmail.com>
2019-11-08 10:15:32 +0700

    sharebuttons

---
+++ src/share-buttons/test-button.vue
@@ -0,0 +1,100 @@
+<template>
+    <div id="share-btns" style="display: flex">
+        <div data-network="twitter" data-title="Denko Sekka" data-url="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8004126-dbe3-44b3-b71f-e122c25f5a8b/dacg968-30cc192a-4278-427f-8e60-03ced74e6fa9.png/v1/fill/w_1024,h_1021,q_80,strp/denko_sekka_by_parrydox_dacg968-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyMSIsInBhdGgiOiJcL2ZcL2I4MDA0MTI2LWRiZTMtNDRiMy1iNzFmLWUxMjJjMjVmNWE4YlwvZGFjZzk2OC0zMGNjMTkyYS00Mjc4LTQyN2YtOGU2MC0wM2NlZDc0ZTZmYTkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.HL8m57WYipFV6p46oK0JKMpoYUkOGw_8NL7mR1OQQsg" class="st-custom-button">
+            <button  
+            style="cursor: pointer; 
+            background-color:#1A91DA; 
+            height: 45px; 
+            width: 50px; 
+            font-size: 22px;
+            color: white;
+            border-radius: 4px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 4px" 
+            type="button" class="btn share-btn"><i class="fab fa-twitter share-icon"></i></button>
+        </div>
+
+        <div data-network="facebook" data-title="Denko Sekka" data-url="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8004126-dbe3-44b3-b71f-e122c25f5a8b/dacg968-30cc192a-4278-427f-8e60-03ced74e6fa9.png/v1/fill/w_1024,h_1021,q_80,strp/denko_sekka_by_parrydox_dacg968-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyMSIsInBhdGgiOiJcL2ZcL2I4MDA0MTI2LWRiZTMtNDRiMy1iNzFmLWUxMjJjMjVmNWE4YlwvZGFjZzk2OC0zMGNjMTkyYS00Mjc4LTQyN2YtOGU2MC0wM2NlZDc0ZTZmYTkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.HL8m57WYipFV6p46oK0JKMpoYUkOGw_8NL7mR1OQQsg" class="st-custom-button">
+            <button  
+            style="cursor: pointer; 
+            background-color:#4267B2; 
+            height: 45px; 
+            width: 50px; 
+            font-size: 22px;
+            color: white;
+            border-radius: 4px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 4px" 
+            type="button" class="btn share-btn"><i class="fab fa-facebook-square"></i></button>
+        </div>
+        
+        <div data-network="pinterest" data-title="Denko Sekka" data-url="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8004126-dbe3-44b3-b71f-e122c25f5a8b/dacg968-30cc192a-4278-427f-8e60-03ced74e6fa9.png/v1/fill/w_1024,h_1021,q_80,strp/denko_sekka_by_parrydox_dacg968-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyMSIsInBhdGgiOiJcL2ZcL2I4MDA0MTI2LWRiZTMtNDRiMy1iNzFmLWUxMjJjMjVmNWE4YlwvZGFjZzk2OC0zMGNjMTkyYS00Mjc4LTQyN2YtOGU2MC0wM2NlZDc0ZTZmYTkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.HL8m57WYipFV6p46oK0JKMpoYUkOGw_8NL7mR1OQQsg" class="st-custom-button">
+            <button  
+            style="cursor: pointer; 
+            background-color:#CB2027; 
+            height: 45px; 
+            width: 50px; 
+            font-size: 22px;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 4px;
+            margin: 4px"
+            type="button" class="btn share-btn"><i class="fab fa-pinterest"></i></button>
+        </div>
+
+        <div data-network="line" data-title="Denko Sekka" data-url="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8004126-dbe3-44b3-b71f-e122c25f5a8b/dacg968-30cc192a-4278-427f-8e60-03ced74e6fa9.png/v1/fill/w_1024,h_1021,q_80,strp/denko_sekka_by_parrydox_dacg968-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyMSIsInBhdGgiOiJcL2ZcL2I4MDA0MTI2LWRiZTMtNDRiMy1iNzFmLWUxMjJjMjVmNWE4YlwvZGFjZzk2OC0zMGNjMTkyYS00Mjc4LTQyN2YtOGU2MC0wM2NlZDc0ZTZmYTkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.HL8m57WYipFV6p46oK0JKMpoYUkOGw_8NL7mR1OQQsg" class="st-custom-button">
+            <button  
+            style="cursor: pointer; 
+            background-color:#42C607; 
+            height: 45px; 
+            width: 50px; 
+            font-size: 25px;
+            color: white;
+            border-radius: 4px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border:1px solid white;
+            margin: 4px" 
+            type="button" class="btn share-btn"><i class="fab fa-line"></i></button>
+        </div>
+
+        <div data-network="gmail" data-title="Denko Sekka" data-url="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8004126-dbe3-44b3-b71f-e122c25f5a8b/dacg968-30cc192a-4278-427f-8e60-03ced74e6fa9.png/v1/fill/w_1024,h_1021,q_80,strp/denko_sekka_by_parrydox_dacg968-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTAyMSIsInBhdGgiOiJcL2ZcL2I4MDA0MTI2LWRiZTMtNDRiMy1iNzFmLWUxMjJjMjVmNWE4YlwvZGFjZzk2OC0zMGNjMTkyYS00Mjc4LTQyN2YtOGU2MC0wM2NlZDc0ZTZmYTkucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.HL8m57WYipFV6p46oK0JKMpoYUkOGw_8NL7mR1OQQsg" class="st-custom-button">
+            <button  
+            style="cursor: pointer; 
+            background-color:#FFFFFF; 
+            height: 45px; 
+            width: 50px; 
+            font-size: 22px;
+            color: black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 4px;
+            border:1px solid black;
+            margin: 4px" 
+            type="button" class="btn share-btn"><i class="fab fa-google"></i></button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            img: ''
+        }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
